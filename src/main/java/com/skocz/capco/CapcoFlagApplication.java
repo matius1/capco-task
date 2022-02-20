8b455f0a7329dd9ce13f239ab23fa8c6bd10fc9a
Mateusz <matius1@o2.pl>
2022-02-20 14:23:24 +0100

    Init commit

---
+++ src/main/java/com/skocz/capco/CapcoFlagApplication.java
@@ -0,0 +1,13 @@
+package com.skocz.capco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapcoFlagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapcoFlagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skocz/capco/CapcoFlagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skocz.capco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapcoFlagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
